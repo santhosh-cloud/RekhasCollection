f488a0f8a98dc74ddab0cac3b243b9e736602334
santhosh-cloud <56725056+santhosh-cloud@users.noreply.github.com>
2022-02-10 15:01:36 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import styles from '../styles/Header.module.css'
 export const Header = () => {
   return (
     <div className={styles.container}>
-      <div>Rekha's Collection</div>
+      <div>Rekhas Collection</div>
     </div>
   )
 }
